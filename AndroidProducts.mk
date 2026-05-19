5a59259ddac436fde7674bbd0eb511f43ddaf6b4
balarama007 <balaramas007@gmail.com>
2026-05-19 22:59:23 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,6 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
+# 
+# MAN IT'S SO HARD🫩
+# 
 #
 
 PRODUCT_MAKEFILES := \
