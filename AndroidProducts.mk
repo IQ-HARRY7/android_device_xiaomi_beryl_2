5451dd4c958c02ce5303e7aedf1402ff0211c31a
balarama007 <balaramas007@gmail.com>
2026-05-20 11:51:03 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
-# 
+# .
 # MAN IT'S SO HARD🫩
-# 
+# ask god how it works, because i dont know. 
+# Created by Claude not me.
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fox_beryl.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_beryl-user \
-    twrp_beryl-userdebug \
-    twrp_beryl-ap2a-eng
+    fox_beryl-ap2a-eng \
+    fox_beryl-ap2a-userdebug
