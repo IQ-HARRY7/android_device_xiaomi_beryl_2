acde052a40c8d25c727ac41c9e091e1ab1ac4604
balarama007 <balaramas007@gmail.com>
2026-05-20 09:38:06 +0530

    Update Android.mk

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -1,9 +1,31 @@
+# ITS HARD GUYS🥲
+#
+# Copyright (C) 2025-2026 OrangeFox Recovery Project
+# SPDX-License-Identifier: GPL-3.0-only
+#
+
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(TARGET_DEVICE),beryl)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := kernel_modules
+LOCAL_MODULE := beryl_kernel_modules
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/lib/modules
-LOCAL_SRC_FILES := vendor/lib/modules/1.1/%.ko
-# ... list all modules
+
+# Copy all .ko files from prebuilt/modules/ to recovery
+KERNEL_MODULES := $(wildcard $(LOCAL_PATH)/modules/*.ko)
+LOCAL_SRC_FILES := $(KERNEL_MODULES)
+
+$(foreach module,$(KERNEL_MODULES),$(eval $(call copy-one-file,$(module),$(TARGET_RECOVERY_ROOT_OUT)/lib/modules/$(notdir $(module)))))
+
+# Copy modules metadata files
+$(call copy-one-file,$(LOCAL_PATH)/modules/modules.alias,$(TARGET_RECOVERY_ROOT_OUT)/lib/modules/modules.alias)
+$(call copy-one-file,$(LOCAL_PATH)/modules/modules.dep,$(TARGET_RECOVERY_ROOT_OUT)/lib/modules/modules.dep)
+$(call copy-one-file,$(LOCAL_PATH)/modules/modules.load,$(TARGET_RECOVERY_ROOT_OUT)/lib/modules/modules.load)
+$(call copy-one-file,$(LOCAL_PATH)/modules/modules.load.recovery,$(TARGET_RECOVERY_ROOT_OUT)/lib/modules/modules.load.recovery)
+$(call copy-one-file,$(LOCAL_PATH)/modules/modules.softdep,$(TARGET_RECOVERY_ROOT_OUT)/lib/modules/modules.softdep)
+
 include $(BUILD_PHONY_PACKAGE)
+
+endif
